51cec4697f25b13cdb3e0d68b9c5813f80cc5e9e
Miles Eng <miles.eng@duke.edu>
2023-11-28 23:15:25 -0500

    added data and initial studentmodel write

---
+++ test.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import numpy as np
+import sklearn
+from sklearn import linear_model
+from matplotlib import style
+import matplotlib.pyplot as pyplot
+import pickle
+
+data = pd.read_csv("data/student-mat.csv", sep=";")
+data = data[["G1", "G2", "G3", "studytime", "failures", "absences"]]
+
+# print(data.head())
+
+predict = "G3"
+
+X = np.array(data.drop([predict], 1))
+Y = np.array(data[predict])
+
+x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, Y, test_size = 0.1)
+
+# linear = linear_model.LinearRegression()
+
+# linear.fit(x_train, y_train)
+
+# # save model
+# with open("data/studentmodel.pickle", "wb") as f:
+#   pickle.dump(linear, f)
+
+pickle_in = open("data/studentmodel.pickle", "rb")
+linear = pickle.load(pickle_in)
+
+accuracy = linear.score(x_test, y_test)
+print("accuracy: " + str(accuracy))
+
+predictions = linear.predict(x_test)
+
+for x in range(len(predictions)):
+  print(predictions[x], x_test[x], y_test[x])
+
+print("coefficient: " + str(linear.coef_))
+print("intercept: " + str(linear.intercept_))
